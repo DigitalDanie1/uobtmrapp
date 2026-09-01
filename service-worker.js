49bd1e9a7b02fc23e4346505a18d4622a390c6ec
Daniel <daniel@Mac.localdomain>
2026-09-01 15:35:54 +0800

    Keep bottom navigation above iOS safe area

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
 const CACHE_PREFIX = "banking-demo-";
-const CACHE_NAME = `${CACHE_PREFIX}v91`;
-const APP_START = "./index.html?app=1&viewer=1&v=91";
+const CACHE_NAME = `${CACHE_PREFIX}v92`;
+const APP_START = "./index.html?app=1&viewer=1&v=92";
 
 const APP_SHELL = [
   "./",
   APP_START,
-  "./manifest.json?v=91",
+  "./manifest.json?v=92",
   "./assets/brand-uob-tmrw-extracted.png",
   "./assets/merchant-insight-art-20260824.png",
   "./assets/closest-icon-180.png",
